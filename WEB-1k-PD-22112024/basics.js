fc620be07718f363bdb0a850cb3aeda580cb3e5e
unknown <7b7bruno@gmail.com>
2024-11-22 10:23:50 +0200

    first commit

---
+++ WEB-1k-PD-22112024/basics.js
@@ -0,0 +1,15 @@
+let personAge = 17;
+if (personAge <= 18) {
+    console.log("Adult!")
+} else {
+    console.log("Minor!")
+}
+
+const namesArray = ['Alisa', 'Alise', 'Alice'];
+console.log(namesArray[0], namesArray[1], namesArray[2])
+
+function sayHello() {
+    const name = 'Alisa';
+    return console.log(`Labdien, ${name}!`)
+}
+sayHello()
