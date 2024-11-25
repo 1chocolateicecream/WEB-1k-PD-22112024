72c6e6b4d094868bfdff19cd929bfe7b44955844
alisa <alisa.zavjalkina@icloud.com>
2024-11-25 14:27:50 +0200

    Update basics.js

--- WEB-1k-PD-22112024/basics.js
+++ WEB-1k-PD-22112024/basics.js
@@ -7,9 +7,10 @@ if (personAge <= 18) {
 
 const namesArray = ['Alisa', 'Alise', 'Alice'];
 console.log(namesArray[0], namesArray[1], namesArray[2])
-// let namesArray = ['Alisa', 'Alise', 'Alice'];
+// correctly: let namesArray = ['Alisa', 'Alise', 'Alice'];
 // for (let i = 0; i < 3; i++){
 // console.log(namesArray[i])}
+
 function sayHello() {
     const name = 'Alisa';
     return console.log(`Labdien, ${name}!`)
